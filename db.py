e3f5e7fe9bed86971f7cd84201fe4b0bea919d5b
apache <alexdev953@gmail.com>
2023-01-30 16:07:27 +0200

    added actual time in group update

--- db.py
+++ db.py
@@ -35,12 +35,14 @@ class DataBase:
             cur.execute('select telegram_id from users where telegram_id = ?', (user.id,))
             info = cur.fetchone()
             if not info:
+                print('insert')
                 cur.execute("insert into users (first_name, user_name, telegram_id, last_name) values (?, ?, ?, ?)",
                             (user.first_name, user.username, user.id, user.last_name))
                 self.con.commit()
             elif info:
+                print('update')
                 cur.execute("update users set updated = current_timestamp, last_name = ? where telegram_id = ?",
-                            (user.id, user.last_name))
+                            (user.last_name, user.id))
                 self.con.commit()
             cur.close()
         except Exception as e:
