de12e298c2004067a16f51e75b69a91b72b029b0
apache <alexdev953@gmail.com>
2025-01-04 22:33:26 +0200

    added performance by redis

--- bot.py
+++ bot.py
@@ -210,7 +210,7 @@ async def take_now_cmd(message: types.Message):
 async def take_help(message: types.Message):
     await message.answer('Якщо у вас є зауваження до роботи бота або побажання по удосконаленню '
                          'напишіть мені використовуючи команду\n👇👇👇👇👇👇👇👇\n/report <i>ТЕКСТ ПОВІДОМЛЕННЯ</i>\n\n'
-                         '<code>ver. 2024.08.005</code>')
+                         '<code>ver. 2025.01.002</code>')
     await actual_info(message)
 
 
